1e3045701160f267dc555c65c9b56e173a880f53
unknown <y.sanghavi001@gmail.com>
2021-03-08 11:39:42 +0530

    Commonnn Input Component Addedd

--- src/containers/Signup/Signup.index.js
+++ src/containers/Signup/Signup.index.js
@@ -1,50 +1,61 @@
-import React from 'react'
+import React from "react";
 import Layout from "./../../components/Layout/Layout.index";
 import { Container, Form, Row, Button, Col } from "react-bootstrap";
+import Input from "./../../components/UI/Input/Input.index";
 function Signup() {
-    return (
-        <Layout>
-        <Container>
-          <Row style={{marginTop:'50px'}}>
-            <Col md={{span:6,offset:3}}>
-              <Form>
-                <Row>
-                    <Col md={6}>
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label>First Name</Form.Label>
-                            <Form.Control type="text" placeholder="Password" />
-                        </Form.Group>
-                    </Col>
+  return (
+    <Layout>
+      <Container>
+        <Row style={{ marginTop: "50px" }}>
+          <Col md={{ span: 6, offset: 3 }}>
+            <Form>
+              <Row>
+                <Col md={6}>
+                  <Input
+                    label="First Name"
+                    placeholder="First Name"
+                    value=""
+                    type="text"
+                    onChange={() => {}}
+                  />
+                </Col>
 
-                    <Col md={6}>
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Last Name</Form.Label>
-                            <Form.Control type="text" placeholder="Password" />
-                        </Form.Group>
-                    </Col>
-                </Row>
+                <Col md={6}>
+                  <Input
+                    label="Last Name"
+                    placeholder="Last Name"
+                    value=""
+                    type="text"
+                    onChange={() => {}}
+                  />
+                </Col>
+              </Row>
 
-                <Form.Group controlId="formBasicEmail">
-                  <Form.Label>Email address</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
-                  <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                  </Form.Text>
-                </Form.Group>
-  
-                <Form.Group controlId="formBasicPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                  Submit
-                </Button>
-              </Form>
-            </Col>
-          </Row>
-        </Container>
-      </Layout>
-    )
+               <Input
+                    label="Email"
+                    placeholder="Email"
+                    value=""
+                    type="email"
+                    onChange={() => {}}
+                />
+
+                <Input
+                    label="Password"
+                    placeholder="Password"
+                    value=""
+                    type="password"
+                    onChange={() => {}}
+                />
+                
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </Layout>
+  );
 }
 
-export default Signup
+export default Signup;

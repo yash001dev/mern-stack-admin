c2e8a2fdb9cadcbd7c24cf85e48fc6f0ab8bafa6
unknown <y.sanghavi001@gmail.com>
2021-03-08 12:16:29 +0530

    Sign in With Input Component Ready

--- src/containers/Signin/Signin.index.js
+++ src/containers/Signin/Signin.index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Layout from "./../../components/Layout/Layout.index";
 import { Container, Form, Row, Button, Col } from "react-bootstrap";
+import Input from "../../components/UI/Input/Input.index";
+
 function Signin() {
   return (
     <Layout>
@@ -8,18 +10,21 @@ function Signin() {
         <Row style={{marginTop:'50px'}}>
           <Col md={{span:6,offset:3}}>
             <Form>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
+              <Input
+                label="Email"
+                placeholder="Email"
+                value=""
+                type="email"
+                onChange={()=>{}}
+              />
 
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
+              <Input
+                  label="Password"
+                  placeholder="Password"
+                  value=""
+                  type="password"
+                  onChange={()=>{}}
+              />
               <Button variant="primary" type="submit">
                 Submit
               </Button>
